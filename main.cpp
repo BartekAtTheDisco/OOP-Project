735af7a1e46c20f6e2bf555ae64278045dc4daae
tobiiwankenobi <64904687+tobiiwankenobi@users.noreply.github.com>
2020-05-07 16:22:12 +0200

    już bardziej finalna postać
    jeszcze nie zrobiłem zabezpieczenia żeby po podanych złych pasów wywalało a nie dawało dostęp do konta

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "account.h"
 #include "wallet.h"
 
@@ -7,10 +8,42 @@ using namespace std;
 int main()
 {
 
+    int log;
     cout<<"              _| Welcome to the cryptocurrency stock simulator |_ "<<endl;
-    cout<<endl<<"   CREATE ACCOUNT"<<endl<<endl<<"      -------"<<endl;
+    cout<<endl<<"1.   SIGN IN";
+    cout<<endl<<"2.   CREATE ACCOUNT"<<endl<<endl<<"      -------"<<endl;
+    cin>>log;
 
+    switch(log)
+    {
+case 1:
+    {
+    string x, y, login, password,line;
+    cout<<endl<<" login: ";
+    cin>>x;
+    cout<<endl<<" password: ";
+    cin>>y;
 
+    fstream file;
+    file.open("profile.txt", ios::in);
+    getline(file,line);
+    login=line;
+    getline(file,line);
+    password=line;
+    file.close();
+    cout<<login;
+    cout<<password;
+    if((login==x)&&(password==y))
+    {
+        cout<<endl<<endl<<"                                                        **access approved**";
+    }
+    else
+        cout<<endl<<endl<<"                                                        **access denied**";
+
+    break;
+    }
+
+case 2:
     string login,password,name,surname;
     cout<<endl<<" enter your login: ";
     cin>>login;
@@ -22,10 +55,7 @@ int main()
     cin>>surname;
     Wallet w1(name,surname,login,password);
     w1.create_account();
-
-
-
-    int choice;
+     int choice;
     cout<<endl<<endl<<"                           ~~~~~~~~~~~~~~~~~~~~";
     cout<<endl<<endl<<"                    ***choose number to pick action***"<<endl;
     cout<<endl<<" 1. show my account data"<<endl;
@@ -37,6 +67,11 @@ int main()
     case 1:
     w1.print_account();
     break;
+
+
+    }
+
+
 
 
 
