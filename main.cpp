cc2ce2957be5cad3755b77eb3988ede8cfb3995c
tobiiwankenobi <64904687+tobiiwankenobi@users.noreply.github.com>
2020-05-13 09:39:59 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,55 +2,59 @@
 #include <fstream>
 #include "account.h"
 #include "wallet.h"
-#include "Functions.hpp"
-#include "Cryptocurrency.hpp"
 
 using namespace std;
 
 int main()
 {
 
-    //Konstruktory krypto(domyślnie każda wartość jest na 0)
-    Bitcoin B;
-    Ethereum E;
-    Lisk L;
-    
-    // przypisywanie wartości do każdej krypto(polimorfizm)
-    Cryptocurrency* ptr{nullptr};
-    ptr = &B;
-    ptr->assign_current_value(check_BTC());
-    ptr = &E;
-    ptr->assign_current_value(check_ETH());
-    ptr = &L;
-    ptr->assign_current_value(check_LSK());
-    
     int log;
+    while(log!=0)
     cout<<"              _| Welcome to the cryptocurrency stock simulator |_ "<<endl;
-    cout<<endl<<"1.   SIGN IN";
-    cout<<endl<<"2.   CREATE ACCOUNT"<<endl<<endl<<"      -------"<<endl;
+    cout<<endl<<" 1.   SIGN IN";
+    cout<<endl<<" 2.   CREATE ACCOUNT";
+    cout<<endl<<" 0.   EXIT"<<endl<<endl<<"      -------"<<endl;
     cin>>log;
 
-    switch(log)
-{
-
-
-case 1:
-{
+    if(log==1)
+    {
     string x, y;
-
     cout<<endl<<" login: ";
     cin>>x;
     cout<<endl<<" password: ";
     cin>>y;
     Wallet w1(x,y);
     w1.sign_in(x,y);
-    w1.print_account(x,y);
+    Account a1(x,y);
 
-}
+    int choice;
+    while(choice!=0)
+    {
+    cout<<endl<<endl<<"                    ***choose number to pick action***"<<endl;
+    cout<<endl<<" 1. show my account data"<<endl;
+    cout<<endl<<" 2. show my balance"<<endl;
+    cout<<endl<<" 3. load EUR to my account"<<endl;
+    cout<<endl<<" 0. log out"<<endl;
+    cin>>choice;
+
+    switch(choice)
+    {
+
+case 1:
+    {
+    w1.print_account();
     break;
+    }
+case 3:
+    {
+    a1.load_eur();
+    }
 
 
-case 2:
+    }
+    }
+    }
+    else if(log==2)
     {
     string login,password,name,surname;
     cout<<endl<<" enter your login: ";
@@ -62,23 +66,12 @@ case 2:
     cout<<endl<<" enter your SURNAME: ";
     cin>>surname;
     Wallet w1(login,password,name,surname);
-
     w1.create_account();
     }
-    break;
-}
-     int choice;
-    cout<<endl<<endl<<"                           ~~~~~~~~~~~~~~~~~~~~";
-    cout<<endl<<endl<<"                    ***choose number to pick action***"<<endl;
-    cout<<endl<<" 1. show my account data"<<endl;
-    cout<<endl<<" 2. show my balance"<<endl;
-    cin>>choice;
-
-
-
-
-
-
+    else if(log!=2 && log!=1 && log!=0)
+    {
+        cout<<endl<<" error";
+    }
 
 
 
