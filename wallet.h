8d0dda4bb5bd6ca9e16b319fe0e6f6edaa945bf8
tobiiwankenobi <64904687+tobiiwankenobi@users.noreply.github.com>
2020-05-13 09:40:56 +0200

    Update wallet.h

--- wallet.h
+++ wallet.h
@@ -4,28 +4,23 @@
 
 
 
-
-
 using namespace std;
 
 class Wallet{
+protected:
     string name_;
     string surname_;
     string login_;
     string password_;
-    float BTC;
-    float ETH;
-    float LSK;
 
 public:
-    void print_account(string login, string password);
+    void print_account();
     void create_account();
     void sign_in(string login, string password);
     Wallet(string login, string password, string name, string surname);
     Wallet(string login, string password);
+
 };
-
-
 
 
 #endif // WALLET_H_INCLUDED
