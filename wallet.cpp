805a3c915c166a60536a4cb5706d01f92eb3778c
tobiiwankenobi <64904687+tobiiwankenobi@users.noreply.github.com>
2020-05-13 09:41:38 +0200

    Update wallet.cpp

--- wallet.cpp
+++ wallet.cpp
@@ -10,27 +10,29 @@ using namespace std;
 
 void Wallet::sign_in(string x, string y)
 {
-    string login, password,line,line1;
+    string login, password,word;
     int check=0;
     fstream file;
     file.open("profile.txt", ios::in);
-    string search_str =x;
-    while(getline(file,line))
+
+    while(file>>word)
     {
-    login=line;
-    if(line.find(search_str) != string::npos)
-    {
-    getline(file,line);
-    password=line;
+    login=word;
+    file>>word;
+    password=word;
 
     if((login==x)&&(password==y))
     {
 
         cout<<endl<<endl<<"                                                  **access approved**";
-        getline(file,line);
+        file>>word;
+        name_=word;
 
-        getline(file,line1);
-        cout<<endl<<endl<<" HELLO "<<line<<" "<<line1<<" :)"<<endl;
+        file>>word;
+        surname_=word;
+        cout<<endl<<endl<<" HELLO "<<name_<<" "<<surname_<<" :)"<<endl;
+        login_=login;
+        password_=password;
         check=1;
     }
 
@@ -38,59 +40,35 @@ void Wallet::sign_in(string x, string y)
 
 
     }
-    }
+
     if(check==0)
          cout<<endl<<endl<<"                                                  **access denied**";
 
     file.close();
+
 }
 
-void Wallet::print_account(string login, string password)
+void Wallet::print_account()
 {
-    fstream file;
-    file.open("profile.txt", ios::in);
-    string search_str = login;
-    string line,line1,line2;
-
-    while(getline(file,line))
-    {
-        line1=line;
-        if(line.find(search_str)!=string::npos)
-        {
-            getline(file,line);
-            line2=line;
-            if(line1==login && line2==password)
-            {
-                getline(file,line);
-                cout<<endl<<line;
-                getline(file,line);
-                cout<<endl<<line;
-            }
-
-
-        }
-    }
-
-
-
-
-
-    file.close();
+    cout<<endl<<"==========ACCOUNT DATA==========";
+    cout<<endl<<" NAME: "<<name_;
+    cout<<endl<<" SURNAME: "<<surname_;
 }
 
 void Wallet::create_account()
 {
-
+    double zero = 0.0;
     fstream file;
     file.open("profile.txt", ios::out | ios::app);
     if(file.good()==true)
     {
 
-        file<<login_<<endl;
-        file<<password_<<endl;
-        file<<name_<<endl;
-        file<<surname_<<endl;
-        file<<'+'<<endl;
+        file<<login_<<" ";
+        file<<password_<<" ";
+        file<<name_<<" ";
+        file<<surname_<<" ";
+        file<<zero<<" "<<endl;
+
     }
     else
         cout<<endl<<" *error*";
@@ -104,11 +82,11 @@ Wallet::Wallet(string login, string password,string name, string surname)
 :login_{login},password_{password},name_{name},surname_{surname}
 {
 
-    cout<<endl<<"constructor W";
+
 }
 
 Wallet::Wallet(string login, string password)
 :login_{login},password_{password},name_{""},surname_{""}
 {
-    cout<<"sign in constructor W";
+
 }
