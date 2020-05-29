27d12a424539a6f77d5c0043f114080c2cab8f5b
tobiiwankenobi <64904687+tobiiwankenobi@users.noreply.github.com>
2020-05-29 15:23:28 +0200

    Update account.cpp

--- account.cpp
+++ account.cpp
@@ -1,33 +1,43 @@
-#include <iostream>
-#include "account.h"
+include "wallet.h"
 #include <fstream>
+#include <sstream>
 
 using namespace std;
 
+Account::Account()
+:eur_{0.0}
+{
 
-void Account::sign_in()
-{/*
-    string login, password;
-    cout<<" login: ";
-    cin>>login;
-    cout<<" password: ";
-    cin>>password;
-
-    if((password==password_)&&(login==login_))
-    {
-
-
-    }
-
-    fstream file;
-    file.open("profile.txt",ios::is);
-    getline(file, first);
-    cout<<fisrt;
-    file.close();*/
 }
 
+/*
 
-void Account::show_balance()
+void Account::load_eur()
+
 {
+    double dep,eur;
+    string word,word1,x;
+
+    cout<<endl<<"How much money do you want to deposit?"<<endl<<"EUR: ";
+    cin>>dep;
+
+
+    fstream file;
+    file.open("profile.txt", ios::in | ios::out);
+    while(file>>word)
+    {
+        file>>word1;
+        if(word==_login && word1==_password)
+        {
+            file>>skipws>>x>>x;
+            file>>eur;
+            eur+=dep;
+            file>>x>>x>>x>>x;
+            file<<eur;
+
+
+        }
+    }
+    file.close();
 
 }
