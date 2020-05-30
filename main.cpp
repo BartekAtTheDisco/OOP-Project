4e45dbc960102331034a1d4edf4ac2a9aba811b2
BartekAtTheDisco <64702330+BartekAtTheDisco@users.noreply.github.com>
2020-05-30 15:08:00 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,53 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <windows.h>
 #include "account.h"
-#include "wallet.h"
 #include "manager.h"
+#include "Cryptocurrency.hpp"
+#include "Functions.hpp"
 
 using namespace std;
 
 int main()
 {
-    int log;
-    bool end = true;
+    Bitcoin BTC;
+    Ethereum ETH;
+    Litecoin LTC;
+    Cryptocurrency* ptr;
+    ptr = &BTC;
+    ptr->assign_current_value(check_BTC());
+    ptr = &ETH;
+    ptr->assign_current_value(check_ETH());
+    ptr = &LTC;
+    ptr->assign_current_value(check_LTC());
     vector <Account> acc;
-    int i=0;
     Manager manage;
-    do
+    
+    
+     while(true)
+        {
+        int choice;
+        cout << "1. Create account" << endl;
+        cout << "2. Sign in" << endl;
+        cin >> choice;
+        switch (choice) {
+            case 1:
+                manage.create_account();
+                break;
+                
+            case 2:
+                acc.emplace_back(manage.sign_in());
+                break;
+        }
+            cout << "brawo mistrzuniu" << endl;
+        }
+    
+        return 0;
+    }
+    //int log;
+    //bool end = false;
+    //int i=0;
+   /* do
     {
     int log;
     cout<<endl<<"              _| Welcome to the cryptocurrency stock simulator |_ "<<endl;
@@ -26,13 +58,11 @@ int main()
 
 
     switch(log)
+    {
     case 1:
     {
-    string login;
-    login = manage.log_in();
-    Account acc[i].emplace_back{Account()};
+        manage.log_in();
 
-    {
     int choice;
     cout<<endl<<endl<<"                    ***choose number to pick action***"<<endl;
     cout<<endl<<" 1. show my account data"<<endl;
@@ -40,24 +70,18 @@ int main()
     cout<<endl<<" 3. load EUR to my account"<<endl;
     cout<<endl<<" 0. log out"<<endl;
     cin>>choice;
-
+        break;
     switch(choice)
     {                                               // tu mają być case'y i trzeba pętle zrobić
 
 
     }
+    
     }
+        case 2:
+            manage.create_account();
+            cout << "gówno";
     }
-    }
-    while(end==true);
-
-
-
-
-
-
-
-
-    return 0;
-}
-
+        
+    }while(end==false);
+*/
