86d895db013936c98f68691756631527ca9f0777
tobiiwankenobi <64904687+tobiiwankenobi@users.noreply.github.com>
2020-06-01 16:04:12 +0200

    poformatowane troche

--- Cryptocurrency.cpp
+++ Cryptocurrency.cpp
@@ -1,38 +1,53 @@
-#include "Cryptocurrency.hpp"
+#include "cryptocurrency.h"
 #include <iostream>
 
 
 
-void Bitcoin::assign_current_value(float x){
+void Bitcoin::assign_current_value(float x)
+{
     current_value = x;
 }
 
-void Ethereum::assign_current_value(float x){
+void Ethereum::assign_current_value(float x)
+{
     current_value = x;
 }
 
-void Litecoin::assign_current_value(float x){
+void Litecoin::assign_current_value(float x)
+{
     current_value = x;
 }
 
-float Bitcoin::get_current_value(){
+float Bitcoin::get_current_value()
+{
     return current_value;
 }
 
-float Ethereum::get_current_value(){
+float Ethereum::get_current_value()
+{
     return current_value;
 }
 
-float Litecoin::get_current_value(){
+float Litecoin::get_current_value()
+{
     return current_value;
 }
 
 
 Bitcoin::Bitcoin()
-    :current_value{0}{cout << "Initialising Bitcoin" << endl;}
+    :current_value{0}
+{
+        cout << "Initialising Bitcoin" << endl;
+}
 
 Ethereum::Ethereum()
-    :current_value{0}{cout << "Initialising Ethereum" << endl;}
+    :current_value{0}
+{
+        cout << "Initialising Ethereum" << endl;
+}
 
 Litecoin::Litecoin()
-    :current_value{0}{cout << "Initialising Lisk" << endl;}
+    :current_value{0}
+{
+        cout << "Initialising Lisk" << endl;
+}
