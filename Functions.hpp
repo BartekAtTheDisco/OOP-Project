8affffac0af4c81bad41ad098885b2e6ba3a418e
BartekAtTheDisco <64702330+BartekAtTheDisco@users.noreply.github.com>
2020-06-04 12:02:07 +0200

    Add files via upload

---
+++ Functions.hpp
@@ -0,0 +1,13 @@
+#ifndef Functions_hpp
+#define Functions_hpp
+
+#include <iostream>
+#include "Cryptocurrency.hpp"
+
+double check_BTC();
+double check_ETH();
+double check_LTC();
+void print_current_value(Cryptocurrency *x);
+
+
+#endif 
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "account.h"
+#include "manager.h"
+#include "Cryptocurrency.hpp"
+#include "Functions.hpp"
+
+using namespace std;
+
+int main()
+{
+    Manager manage;
+    manage.run();
+    return 0;
+}
+
